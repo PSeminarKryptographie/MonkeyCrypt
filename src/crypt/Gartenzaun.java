8ce9247844083571379f2634cb4689787fc72091
heptocat <heptocat@gmail.com>
2017-08-11 12:57:35 +0200

    minor typo correction in Permutation.java, programmed Crypt.Gartenzaun#encrypt() and #verify()

--- src/crypt/Gartenzaun.java
+++ src/crypt/Gartenzaun.java
@@ -14,8 +14,37 @@ public class Gartenzaun extends Chiffre{
 	private List<Integer> length = Arrays.asList(0);
 	@Override
 	public String encrypt(String text, String key) {
-		// TODO Auto-generated method stub
-		return "Noch zu implementieren";
+		String verified = verify(key, "");
+		
+		if (verified == null){
+			int depth = Integer.valueOf(key);
+			if (depth > text.length()){
+				depth = text.length();
+			}
+			System.out.println(depth);
+			int counter = 0;
+			String[] newTextArray = new String[depth];
+			for (int i = 0; i<text.length(); i++){
+				if (i<depth){
+					newTextArray[counter] = "";
+				}
+				newTextArray[counter] += text.charAt(i);
+				if (counter == depth-1){
+					counter = 0;
+				}	
+				else{
+					counter++;
+				}	
+			}
+			StringBuilder builder = new StringBuilder();
+			for(String s : newTextArray) {
+				builder.append(s);
+			}	
+			return builder.toString();
+		}
+		else{
+			return verified;
+		}
 	}
 
 	@Override
@@ -26,8 +55,20 @@ public class Gartenzaun extends Chiffre{
 
 	@Override
 	protected String verify(String key, String alphabet) {
-		// TODO Auto-generated method stub
-		return null;
+		int k;
+		try{
+			k = Integer.valueOf(key);
+		}
+		catch(Exception e){
+			k = -1;
+		}
+		if (k>=0){
+			return null;
+		}
+		else{
+			return "Vorsicht! Bei der Gartenzaun-Transposition muss der Schlüssel eine Natürliche Zahl sein!";
+		}
+		
 	}
 
 }
