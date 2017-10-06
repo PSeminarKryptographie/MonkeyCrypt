1f6ff10f3a5efd69c481b0eb9f1ad20751e16ebd
heptocat <heptocat@gmail.com>
2017-10-06 16:52:59 +0200

    (J) worked on aesthetics, added all the pictures

--- src/core/Core.java
+++ src/core/Core.java
@@ -9,7 +9,7 @@ import crypt.*;
 public class Core
 {
     /**Feld für die Chiffrierungen*/
-    Chiffre[] chiffre = {new Caesar(), new Multiplikativ(), new Vigenere(), new OneTimePad(), new Spaltentransposition(), new Gartenzaun(), new ADFGX(), new Hill(), new Permutation(), new Affine()};
+    Chiffre[] chiffre = {new Caesar(), new Multiplikativ(), new Vigenere(), new OneTimePad(), new Spaltentransposition(), new Gartenzaun(), new ADFGX(), new Hill(), new Permutation(), new Bacon(), new Affine()};
     /**Feld für Spielsprachen*/
     Spielsprache[] spielsprache = {new Anagramm(), new Morse(), new ASCII_Hex(), new ASCII_Bin(), new AlphaIndex(), new BI_language(), new Bobsprache(), new Loeffelsprache(), new BinHex(), new NATO_Alphabet(), new Pseudo_Text()};
 
